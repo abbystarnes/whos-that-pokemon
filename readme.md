1be58e3da6d786e03b394c18db4774356cc55bcf
Wes Reid <bwreid@gmail.com>
2017-03-26 14:08:06 -0700

    Initial commit with base code

---
+++ readme.md
@@ -0,0 +1,5 @@
+## Who's That Pokemon?
+
+Simple application using the [PokemonAPI](http://pokeapi.co).
+
+![https://gyazo.com/992dd402497504c3dac8ba18d710ea20](https://i.gyazo.com/992dd402497504c3dac8ba18d710ea20.gif)
---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+(function () {
+  /*
+    If an argument is passed in to the randomPokemonURL function when invoked, the `all` variable will be set to use that value; however, if no value is entered it will default to true. This is a feature of ES6 called default parameters:
+    https://mzl.la/2nqdxUs
+  */
+
+  function randomPokemonURL (all=true) {
+    var bounds = all ? 720 : 151
+    var id = Math.ceil(Math.random() * bounds)
+    return `http://pokeapi.co/api/v2/pokemon/${id}/`
+  }
+})()
