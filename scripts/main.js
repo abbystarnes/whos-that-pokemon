805ad3d56cde9d7f3dfa54cd39836812332c568b
Wes Reid <bwreid@gmail.com>
2017-03-26 14:08:23 -0700

    potential solution

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 (function () {
+  var name = 'ok'
+  var successClasses = 'green lighten-2 white-text'
+
   /*
     If an argument is passed in to the randomPokemonURL function when invoked, the `all` variable will be set to use that value; however, if no value is entered it will default to true. This is a feature of ES6 called default parameters:
     https://mzl.la/2nqdxUs
@@ -9,4 +12,39 @@
     var id = Math.ceil(Math.random() * bounds)
     return `http://pokeapi.co/api/v2/pokemon/${id}/`
   }
+
+  $('button.get-pokemon').on('click', function () {
+    var url = randomPokemonURL(false)
+
+    $.get(url).then(function (result) {
+      name = result.name[0].toUpperCase() + result.name.slice(1)
+      var sprite = result.sprites.front_default
+      var $img = $(`<img class="hidden" src="${sprite}"/>`)
+      $('.pokemon').empty().append($img)
+      $('.pokemon-name').removeClass(successClasses).val('').attr('disabled', false)
+    }).catch(function (error) {
+      console.error(error)
+    })
+  })
+
+  $('.pokemon-name').on('keyup', function (event) {
+    var $name = $(event.target)
+    var correctName = $name.val().trim().toLowerCase() === name.toLowerCase()
+
+    if (correctName) {
+      $('.pokemon img').removeClass('hidden')
+      $name.addClass(successClasses)
+      $name.attr('disabled', true)
+    } else {
+      $name.removeClass(successClasses)
+    }
+  })
+
+  $('.give-up').on('click', function () {
+    $('.pokemon img').removeClass('hidden')
+
+    var $name = $('.pokemon-name')
+    $name.val(name)
+    $name.attr('disabled', true)
+  })
 })()
