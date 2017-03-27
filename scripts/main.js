b708154f0b36b6015acf1dc4531e15d0107e51bb
Wes Reid <bwreid@gmail.com>
2017-03-27 09:43:36 -0700

    Minor refactor

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,10 @@
   var name = 'ok'
   var successClasses = 'green lighten-2 white-text'
 
+  $('button.get-pokemon').on('click', requestNewPokemon)
+  $('.pokemon-name').on('keyup', validateUserInput)
+  $('.give-up').on('click', giveUp)
+
   /*
     If an argument is passed in to the randomPokemonURL function when invoked, the `all` variable will be set to use that value; however, if no value is entered it will default to true. This is a feature of ES6 called default parameters:
     https://mzl.la/2nqdxUs
@@ -13,22 +17,25 @@
     return `http://pokeapi.co/api/v2/pokemon/${id}/`
   }
 
-  $('button.get-pokemon').on('click', function () {
+  function requestNewPokemon () {
     var url = randomPokemonURL(false)
 
     $.get(url).then(function (result) {
       name = result.name[0].toUpperCase() + result.name.slice(1)
+
       var sprite = result.sprites.front_default
       var $img = $(`<img class="hidden" src="${sprite}"/>`)
+
       $('.pokemon').empty().append($img)
       $('.pokemon-name').removeClass(successClasses).val('').attr('disabled', false)
     }).catch(function (error) {
       console.error(error)
+      alert('Oops! Something went wrong trying to get a new Pokemon. Please try again.')
     })
-  })
+  }
 
-  $('.pokemon-name').on('keyup', function (event) {
-    var $name = $(event.target)
+  function validateUserInput (inputEvent) {
+    var $name = $(inputEvent.target)
     var correctName = $name.val().trim().toLowerCase() === name.toLowerCase()
 
     if (correctName) {
@@ -38,13 +45,13 @@
     } else {
       $name.removeClass(successClasses)
     }
-  })
+  }
 
-  $('.give-up').on('click', function () {
+  function giveUp () {
     $('.pokemon img').removeClass('hidden')
 
     var $name = $('.pokemon-name')
     $name.val(name)
     $name.attr('disabled', true)
-  })
+  }
 })()
